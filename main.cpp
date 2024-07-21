7aeefec61bc3d281823d3aa67f5885c540aa5435
ohara <ohara@g.neec.ac.jp>
2024-07-21 09:54:10 +0900

    プロジェクト ファイルを追加します。

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <Windows.h>
+
+// Windowsアプリでのエントリーポイント(main関数)
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
+//int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_  HINSTANCE, LPSTR, int) {
+
+// 出力ウィンドウへの文字出力
+    OutputDebugStringA("aaa Hello,DirectX!\n");
+
+    return 0;
+}
+
+
