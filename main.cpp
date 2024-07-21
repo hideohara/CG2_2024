c2367bc08d010a670334d07fa2ce3e942b874f51
ohara <ohara@g.neec.ac.jp>
2024-07-21 10:02:14 +0900

    00_02 警告を放置しないようにしよう

--- main.cpp
+++ main.cpp
@@ -5,9 +5,11 @@
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 //int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_  HINSTANCE, LPSTR, int) {
-
+    
+    //int a = 5;
+    //float b = a;
 // 出力ウィンドウへの文字出力
-    OutputDebugStringA("aaa Hello,DirectX!\n");
+    OutputDebugStringA("Hello, DirectX!\n");
 
     return 0;
 }
